3a84c4a232f291537d8cc78a39bb67acbb2709ce
maru008 <nydsa529422@gmail.com>
2022-03-07 22:44:06 +0900

    推薦システムの追加

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+import numpy as np
+from scipy.stats import zscore
+import pandas as pd
 
 app= Flask(__name__)
 
@@ -11,15 +14,33 @@ def index():
 
 @app.route("/index",methods=["post"])
 def post():
-    Aroma = request.form.get("Aroma")
-    Flavor = request.form.get("Flavor")
-    Aftertaste = request.form.get("Aftertaste")
-    Acidity = request.form.get("Acidity")
-    Sweetness = request.form.get("Sweetness")
+    #ユーザ入力の値を受け取る
+    Aroma = float(request.form.get("Aroma"))
+    Flavor = float(request.form.get("Flavor"))
+    Aftertaste = float(request.form.get("Aftertaste"))
+    Acidity = float(request.form.get("Acidity"))
+    Sweetness = float(request.form.get("Sweetness"))
     
-    res_datas = [Aroma,Flavor,Aftertaste,Acidity,Sweetness]
+    user_coffee_val = [Aroma,Flavor,Aftertaste,Acidity,Sweetness]
+    # ユーザが選んだコーヒー変数を正規化
+    norm_user_coffee_val = np.array(zscore(user_coffee_val))
     
-    return render_template("index.html", okyo=res_datas)
+    data = pd.read_csv("app/static/data/norm_data.csv",index_col=0)
+    
+    res_df = pd.DataFrame(columns=["Species","Country","error"])
+    for row in data.index:
+        Species = list(data.loc[row])[0]
+        Country = list(data.loc[row])[1]
+        x_ls = np.array(list(data.loc[row])[2:2+len(norm_user_coffee_val)])
+        
+        coffee_error_ls = abs(norm_user_coffee_val - x_ls)
+        coffee_error_sum = sum(coffee_error_ls)
+        add_ls = [Species,Country,coffee_error_sum]
+        res_df.loc[row] = add_ls
+    sort_res_df = res_df.sort_values('error')
+    
+    print(sort_res_df["error"])
+    return render_template("index.html", res = sort_res_df["Country"])
 
 
 #おまじない
